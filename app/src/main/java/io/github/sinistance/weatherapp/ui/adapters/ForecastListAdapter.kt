b09ef2ee1d0f448660043764120d552de8bc1037
sinistance <sinistance@gmail.com>
2017-07-16 23:23:06 +1000

    page 44

---
+++ app/src/main/java/io/github/sinistance/weatherapp/ui/adapters/ForecastListAdapter.kt
@@ -0,0 +1,23 @@
+package io.github.sinistance.weatherapp.ui.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by suryadarma on 16/7/17.
+ */
+class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(TextView(parent.context))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
